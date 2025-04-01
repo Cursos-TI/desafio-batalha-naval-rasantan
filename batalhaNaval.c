1d37a02b9df6b98790fb9c077aa54dc9962eb6ee
Raildo Silva <raildo.silva@sga.pucminas.br>
2025-04-01 03:33:23 +0000

    nivel aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,31 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    int tabuleiro[10][10] = {0}; // Tabuleiro 10x10 inicializado com água (0)
+    int tabuleiro[10][10] = {0};
 
-    // Posicionar 4 navios de tamanho 3 (2 horizontal, 2 vertical)
-    // Navio 1: Horizontal na linha 2, colunas 0-2
+    // Navios horizontais/verticais (como no nível novato)
+    for(int i = 0; i < 3; i++) tabuleiro[2][i] = 3;
+    for(int i = 3; i <= 5; i++) tabuleiro[i][5] = 3;
+    for(int i = 1; i <= 3; i++) tabuleiro[7][i] = 3;
+    for(int i = 0; i <= 2; i++) tabuleiro[i][8] = 3;
+
+    // Navios diagonais
+    // Navio 5: Diagonal (0,0) -> (2,2)
     for(int i = 0; i < 3; i++) {
-        tabuleiro[2][i] = 3;
+        tabuleiro[i][i] = 3;
     }
 
-    // Navio 2: Vertical na coluna 5, linhas 3-5
-    for(int i = 3; i <= 5; i++) {
-        tabuleiro[i][5] = 3;
-    }
-
-    // Navio 3: Horizontal na linha 7, colunas 1-3
-    for(int i = 1; i <= 3; i++) {
-        tabuleiro[7][i] = 3;
-    }
-
-    // Navio 4: Vertical na coluna 8, linhas 0-2
-    for(int i = 0; i <= 2; i++) {
-        tabuleiro[i][8] = 3;
+    // Navio 6: Diagonal (5,3) -> (7,5)
+    for(int i = 0; i < 3; i++) {
+        tabuleiro[5+i][3+i] = 3;
     }
 
     // Exibir tabuleiro
-    printf("Tabuleiro 10x10 (0=Água, 3=Navio):\n");
+    printf("Tabuleiro 10x10 com navios diagonais:\n");
     for(int i = 0; i < 10; i++) {
         for(int j = 0; j < 10; j++) {
             printf("%d ", tabuleiro[i][j]);
